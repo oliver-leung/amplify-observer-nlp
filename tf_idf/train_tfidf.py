2b618fbc9c3edd8827176532c9f393ec3b3d8b2b
Oliver Leung <oal22@cornell.edu>
2021-07-27 20:20:23 +0000

    Set up training script

--- tf_idf/train_tfidf.py
+++ tf_idf/train_tfidf.py
@@ -8,37 +8,44 @@ from sklearn.pipeline import Pipeline
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    
+    # Set hyperparameters
+    parser.add_argument("--n-best", type=int, default=10)
 
     # Sagemaker specific arguments. Defaults are set in the environment variables.
     parser.add_argument("--output-data-dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
     parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
     parser.add_argument("--train", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
 
-    args = parser.parse_args()
+    # Grab args
+    args,_ = parser.parse_known_args()
+    n_best = args.n_best
 
-    # Take the set of files and read them all into a single pandas dataframe
-    input_files = [os.path.join(args.train, file) for file in os.listdir(args.train)]
-    if len(input_files) == 0:
-        raise ValueError(
-            (
-                "There are no files in {}.\n"
-                + "This usually indicates that the channel ({}) was incorrectly specified,\n"
-                + "the data specification in S3 was incorrectly specified or the role specified\n"
-                + "does not have permission to access the data."
-            ).format(args.train, "train")
+    # Select corpus and labels
+    filenames = os.listdir(args.train)
+    files = [os.path.join(args.train, fn) for fn in filenames]
+    dfs = [pd.read_parquet(file, engine='pyarrow') for file in files]
+    train_table = pd.concat(
+            dfs,
+            ignore_index=True
         )
-    raw_data = [pd.read_csv(file, header=None, engine="python") for file in input_files]
-    train_data = pd.concat(raw_data)
 
-    # labels are in the first column
-    train_y = train_data
-
-    # Here we support a single hyperparameter, 'max_leaf_nodes'. Note that you can add as many
-    # as your training my require in the ArgumentParser above.
-    max_leaf_nodes = args.max_leaf_nodes
-
-    # Now use scikit-learn's decision tree classifier to train the model.
-    clf = Pipeline()
+    # Clear empty values and reset indices
+    train_table = train_table[(not isinstance(df.bodyText, str)) and (df.bodyText != '')]
+    train_table = train_table.reset_index(drop=True)
+    train_X = train_table['bodyText']
+    train_y = train_table['url']
+    
+    # Create and train pipeline
+    clf = make_pipeline(
+        TfidfVectorizer(
+            tokenizer=LemmaTokenizer(custom=True)
+        ),
+        VectorSimilarity(
+            n_best=n_best
+        )
+    )
+    clf.fit(train_X, train_y)
 
     # Print the coefficients of the trained classifier, and save the coefficients
     joblib.dump(clf, os.path.join(args.model_dir, "model.joblib"))
